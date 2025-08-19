6f2f24b7ecc1f223d7a0bceb1ec7421e11c79d2f
adarshmoger <adarshmoger2000@gmail.com>
2025-08-19 16:58:19 +0530

    collection of .tf files to create a postgreSQL dtabase in azure cloud

---
+++ output.tf
@@ -0,0 +1,13 @@
+output "resource_group_name" {
+value = azurerm_resource_group.rg.name
+}
+
+output "resource_group_location" {
+value = azurerm_resource_group.rg.location
+}
+
+output "postgres_connection_string" {
+  value = "postgresql://${azurerm_postgresql_flexible_server.post.administrator_login}:${var.admin_pass}@${azurerm_postgresql_flexible_server.post.fqdn}:5432/${azurerm_postgresql_flexible_server_database.postdb.name}"
+  sensitive = true
+}
+
