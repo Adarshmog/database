5c1b288240d4a8c2139f3ab178813621909ae885
adarshmoger <adarshmoger2000@gmail.com>
2025-08-19 14:28:41 +0530

    postgreSQL creation on azure cloud

---
+++ resource.tf
@@ -0,0 +1,26 @@
+resource "azurerm_resource_group" "rg" {
+  name     = var.rg_name
+  location = var.location
+}
+
+resource "azurerm_postgresql_flexible_server" "post" {
+  name                   = var.post
+  resource_group_name    = azurerm_resource_group.rg.name
+  location               = azurerm_resource_group.rg.location
+  version                = "12"
+  administrator_login    = var.admin_name
+  administrator_password = var.admin_pass
+  storage_mb             = 32768
+  sku_name               = "GP_Standard_D2s_v3"
+}
+
+resource "azurerm_postgresql_flexible_server_database" "postdb" {
+  name      = var.postdb
+  server_id = azurerm_postgresql_flexible_server.post.id
+  collation = "en_US.utf8"
+  charset   = "UTF8"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,30 @@
+variable "rg_name" {
+  type    = string
+  default = "MARS"
+}
+
+variable "location" {
+  type    = string
+  default = "westus2"
+}
+
+variable "post" {
+  type    = string
+  default = "adarshpostsql"
+}
+
+variable "admin_name" {
+  type    = string
+  default = "adarsh"
+}
+
+variable "admin_pass" {
+  type    = string
+  default = "maxwell@007"
+}
+
+variable "postdb" {
+  type    = string
+  default = "adarshmaxwell"
+}
+
