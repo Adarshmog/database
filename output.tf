fffea1d2fa3cae22bd95fade9aac2757e10fbf2d
adarshmoger <adarshmoger2000@gmail.com>
2025-08-19 20:23:01 +0530

    creating a new PostgreSQLdatabase

--- output.tf
+++ output.tf
@@ -8,6 +8,5 @@ value = azurerm_resource_group.rg.location
 
 output "postgres_connection_string" {
   value = "postgresql://${var.admin_name}:${var.admin_pass}@${azurerm_postgresql_flexible_server.post.fqdn}:5432/${azurerm_postgresql_flexible_server_database.postdb.name}"
-  sensitive = true
 }
 
